33e2cd23c9e52be41f8f29f2a2e73794ad2bfd94
Thorgaran <39826071+Thorgaran@users.noreply.github.com>
2020-08-10 19:54:20 +0200

    Fix typos and update Ships to Moving Fleets

--- class_Universe.py
+++ class_Universe.py
@@ -6,37 +6,36 @@
 #  
 
 from class_Planet import *
-from class_Ship import *
+from class_MovingFleet import *
 
-class Univers():
+class Universe():
     """
-    A Univers is define by
+    A Universe is defined by
     - a size for the grid (from 0 to x-1 and y-1) -> int
     - some planets -> [class Planet]
-    - some ships -> [class Ship]
+    - some moving fleets -> [class MovingFleet]
     - a time -> int
 
     Methods
-    - a destination -> class planet
-    - an arrival time -> int
+    - big bang -> object initialization
     """
-    def __init__(self, size=None, planets=None, ships=None, time=None):
+    def __init__(self, size=None, planets=None, moving_fleets=None, time=None):
         self.size = size
         self.planets = planets
-        self.ships = ships
+        self.moving_fleets = moving_fleets
         self.time = time
 
     def big_bang(self, size, nb_planets, size_planet_max=3):
         """
-        Function to initialize the univers : its planets and the time.
-        The number ot planet can't exceed size²/2, otherwise an error is raised.
+        Function to initialize the universe : its planets and the time.
+        The number ot planets can't exceed size²/2, otherwise an error is raised.
 
-        nb_planets = nb of planets in the univers
+        nb_planets = nb of planets in the universe
         """
         from random import randint
 
         if nb_planets > size**2 / 2:
-            raise ValueError("Too many planets !")
+            raise ValueError("Too many planets!")
 
         # place the planets
         occupied_position = [(None, None)]
