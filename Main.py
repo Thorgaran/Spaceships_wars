5de86f2e7bddb927a13514b955c5351af6d7ea10
Vicvain <vicvain2002-github@yahoo.com>
2020-08-11 18:50:07 +0200

    Serialisation in main.py

--- Main.py
+++ Main.py
@@ -11,8 +11,10 @@ from class_Universe import *
 from class_Player import *
 from class_GUI import *
 
+from json import dumps, loads
+
 # nb max of turns
-COUNTER_MAX = 1000
+COUNTER_MAX = 10
 
 def display_universe(universe):
     gui = GUI(universe.size)
@@ -48,25 +50,49 @@ def display_universe(universe):
 universe = Universe()
 universe.big_bang(size=10, nb_planets=10, size_planet_max=3, coef_production=1, coef_max_ships=10, nb_players=2)
 
-universe.take_off(
-    planet=universe.planets[0],
-    destination=universe.planets[8],
-    nb_ships=universe.planets[0].nb_ships,
-    speed=2)
-display_universe(universe)
-for i in range(10):
-    universe.next_turn()
-    display_universe(universe)
+# universe.take_off(
+#     planet=universe.planets[0],
+#     destination=universe.planets[8],
+#     nb_ships=universe.planets[0].nb_ships,
+#     speed=2)
+# display_universe(universe)
+# for i in range(10):
+#     universe.next_turn()
+#     display_universe(universe)
 
 # beginning of the game
 counter = 0
 while (universe.winner is None) and (counter < COUNTER_MAX):
     counter += 1
     # serialisation of the univers
-    pass
+    list_planets = [
+        {
+            "x":p.x, "y":p.y,
+            "size":p.size,
+            "production_per_turn":p.production_per_turn,
+            "nb_max_ships":p.nb_max_ships,
+            "owner":p.owner.color,
+            "nb_ships":p.nb_ships
+            }
+            for p in universe.planets
+    ]
+    list_fleets = [
+        {
+            "starting":f.starting_planet,
+            "destination":f.destination_planet,
+            "owner":f.owner.color,
+            "nb_ships":f.nb_ships
+        }
+        for f in universe.fleets
+    ]
+    data = {"planets":list_planets, "fleets":list_fleets}
+    data_string = dumps(data)
+    # print(data_string)
+    
     # get moves player 1 to n
     pass
+
     # play moves
     pass
 
-print(f"The winner is {universe.winner.color}")
+# print(f"The winner is {universe.winner.color}")
